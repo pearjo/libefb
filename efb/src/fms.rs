031153fd66e858206749fe878e57ea9cc0da80ad
Joe Pearson <pearjo@protonmail.com>
2024-09-02 22:24:47 +0200

    fms: Clean-up and add parser for navigation data
    Moves functionality to where it's operating on and adds a function to find fix
    points within the navigation data.
    
    Signed-off-by: Joe Pearson <pearjo@protonmail.com>

--- efb/src/fms.rs
+++ efb/src/fms.rs
@@ -13,59 +13,29 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::airspace::{Airspace, Airspaces};
-use crate::geometry::{point_in_polygon, Coordinate};
-use crate::nd::{Airspace, Waypoint, NavigationData};
+use crate::nd::*;
+use crate::parser::{Arinc424Parser, OpenAirParser, Parser};
 
+#[derive(Debug)]
+pub struct FMSError;
+
+#[repr(C)]
 pub struct FMS {
     pub navigation_data: NavigationData,
 }
 
 impl FMS {
-    /// Returns all [Airspace] at the `point`.
-    pub fn at(&self, point: &Coordinate) -> Vec<&Airspace> {
-        self.navigation_data
-            .airspaces
-            .iter()
-            .filter(|airspace| point_in_polygon(point, &airspace.polygon))
-            .collect()
+    pub fn from_arinc424(path: &str) -> Result<Self, FMSError> {
+        match Arinc424Parser::read(path) {
+            Ok(navigation_data) => Ok(Self { navigation_data }),
+            _ => Err(FMSError),
+        }
     }
-}
 
-#[cfg(test)]
-mod tests {
-    use crate::coord;
-    use crate::geometry::VerticalDistance;
-    use crate::nd::*;
-    use crate::polygon;
-
-    use super::*;
-
-    #[test]
-    fn airspace_at_point() {
-        let inside = coord!(53.03759, 9.00533);
-        let outside = coord!(53.04892, 8.90907);
-
-        let fms = FMS {
-            navigation_data: NavigationData {
-                airspaces: vec![Airspace {
-                    name: String::from("TMA BREMEN A"),
-                    class: AirspaceClass::D,
-                    ceiling: VerticalDistance::Fl(65),
-                    floor: VerticalDistance::Msl(1500),
-                    polygon: polygon![
-                        (53.10111, 8.974999),
-                        (53.102776, 9.079166),
-                        (52.97028, 9.084444),
-                        (52.96889, 8.982222),
-                        (53.10111, 8.974999)
-                    ],
-                }],
-                waypoints: Waypoints::new(),
-            },
-        };
-
-        assert_eq!(fms.at(&inside), vec![&fms.navigation_data.airspaces[0]]);
-        assert!(fms.at(&outside).is_empty());
+    pub fn from_openair(path: &str) -> Result<Self, FMSError> {
+        match OpenAirParser::read(path) {
+            Ok(navigation_data) => Ok(Self { navigation_data }),
+            _ => Err(FMSError),
+        }
     }
 }
--- efb/src/nd/mod.rs
+++ efb/src/nd/mod.rs
@@ -13,6 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use crate::geometry::{point_in_polygon, Coordinate};
+
 mod airac_cycle;
 mod airspace;
 mod mag_var;
@@ -23,8 +25,72 @@ pub use airspace::{Airspace, AirspaceClass, Airspaces};
 pub use mag_var::MagneticVariation;
 pub use waypoint::{Region, Waypoint, WaypointUsage, Waypoints};
 
+#[repr(C)]
+pub enum Fix<'a> {
+    Waypoint(&'a Waypoint),
+}
+
+impl Fix<'_> {
+    fn coordinate(&self) -> Coordinate {
+        match self {
+            Self::Waypoint(wp) => wp.coordinate,
+        }
+    }
+}
+
 #[derive(Default)]
 pub struct NavigationData {
     pub airspaces: Airspaces,
     pub waypoints: Waypoints,
+}
+
+impl NavigationData {
+    pub fn at(&self, point: &Coordinate) -> Vec<&Airspace> {
+        self.airspaces
+            .iter()
+            .filter(|airspace| point_in_polygon(point, &airspace.polygon))
+            .collect()
+    }
+
+    pub fn find<'a>(&self, ident: &str) -> Option<Fix> {
+        self.waypoints
+            .iter()
+            .find(|&wp| wp.route_ident() == ident)
+            .map(|wp| Fix::Waypoint(wp))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::coord;
+    use crate::geometry::VerticalDistance;
+    use crate::polygon;
+
+    use super::*;
+
+    #[test]
+    fn airspace_at_point() {
+        let inside = coord!(53.03759, 9.00533);
+        let outside = coord!(53.04892, 8.90907);
+
+        let nd = NavigationData {
+            airspaces: vec![Airspace {
+                name: String::from("TMA BREMEN A"),
+                class: AirspaceClass::D,
+                ceiling: VerticalDistance::Fl(65),
+                floor: VerticalDistance::Msl(1500),
+                polygon: polygon![
+                    (53.10111, 8.974999),
+                    (53.102776, 9.079166),
+                    (52.97028, 9.084444),
+                    (52.96889, 8.982222),
+                    (53.10111, 8.974999)
+                ],
+            }],
+            waypoints: Waypoints::new(),
+        };
+
+        assert_eq!(nd.at(&inside), vec![&nd.airspaces[0]]);
+        assert!(nd.at(&outside).is_empty());
+    }
 }
