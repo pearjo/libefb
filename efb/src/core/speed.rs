b731211aa08a486c0445ba4034d6e8f5d49cdc46
Joe Pearson <pearjo@protonmail.com>
2024-10-27 15:57:01 +0100

    core: Parse Speed from ICAO flight plan format
    Signed-off-by: Joe Pearson <pearjo@protonmail.com>

--- efb/src/core/speed.rs
+++ efb/src/core/speed.rs
@@ -14,14 +14,16 @@
 // limitations under the License.
 
 use crate::error::Error;
+
 use std::fmt;
 use std::str::FromStr;
 
 /// The speed in either nautical or metrical units.
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq, Debug)]
 pub enum Speed {
     Knots(f32),
     MeterPerSecond(f32),
+    Mach(f32),
 }
 
 impl Speed {
@@ -30,6 +32,7 @@ impl Speed {
         match self {
             Self::Knots(value) => value,
             Self::MeterPerSecond(value) => value,
+            Self::Mach(value) => value,
         }
     }
 
@@ -38,6 +41,7 @@ impl Speed {
         match self {
             Self::Knots(_) => self,
             Self::MeterPerSecond(value) => Self::Knots(value * 1.943844),
+            _ => unimplemented!(),
         }
     }
 
@@ -46,6 +50,7 @@ impl Speed {
         match self {
             Self::Knots(value) => Self::MeterPerSecond(value * 0.514444),
             Self::MeterPerSecond(_) => self,
+            _ => unimplemented!(),
         }
     }
 }
@@ -53,15 +58,28 @@ impl Speed {
 impl FromStr for Speed {
     type Err = Error;
 
+    /// Parses a string `s` to return Speed.
+    ///
+    /// The string should be according to ICAO Doc. 4444 Annex 2:
+    /// - Kilometers per hour, expressed as K followed by 4 figures e.g. `K0830`
+    /// - Knots, expressed as N followed by 4 figures e.g. `N0485`
+    /// - Mach, expressed as M followed by 3 figures to the nearest hundredth of
+    ///   unit Mach e.g. `M082`
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if s.len() == 5 && &s[3..5] == "KT" {
-            if let Ok(speed) = s[0..3].parse::<f32>() {
-                Ok(Speed::Knots(speed))
-            } else {
-                Err(Error::UnexpectedString)
-            }
-        } else {
-            Err(Error::UnexpectedString)
+        macro_rules! value {
+            ($s:expr, $index:expr) => {
+                $s.get($index)
+                    .and_then(|s| s.parse::<u16>().ok()) // ensure that no dot is within the digit
+                    .map(|value| value as f32)
+                    .ok_or(Error::UnexpectedString)
+            };
+        }
+
+        match s.get(0..1).unwrap_or_default() { // first character is the unit
+            "K" => Ok(Speed::MeterPerSecond(value!(s, 1..5)? / 3.6)), // value in km/h
+            "N" => Ok(Speed::Knots(value!(s, 1..5)?)),
+            "M" => Ok(Speed::Mach(value!(s, 1..4)? / 100.0)), // value in hundredth of unit Mach
+            _ => Err(Error::UnexpectedString),
         }
     }
 }
@@ -71,6 +89,20 @@ impl fmt::Display for Speed {
         match self {
             Self::Knots(value) => write!(f, "{value:.0} kt"),
             Self::MeterPerSecond(value) => write!(f, "{value:.0} m/s"),
+            Self::Mach(value) => write!(f, "M{value:.3}"),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn from_str() {
+        assert_eq!("K0360".parse::<Speed>(), Ok(Speed::MeterPerSecond(100.0)));
+        assert_eq!("N0485".parse::<Speed>(), Ok(Speed::Knots(485.0)));
+        assert_eq!("M082".parse::<Speed>(), Ok(Speed::Mach(0.82)));
+        assert_eq!("M08".parse::<Speed>(), Err(Error::UnexpectedString));
     }
 }
--- efb/src/error.rs
+++ efb/src/error.rs
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Error {
     ExpectedWindOnLeg,
     ExpectedSpeedOnLeg,
