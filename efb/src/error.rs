730073ab2d040bbb0afd4d2afd92de829b690e6f
Joe Pearson <pearjo@protonmail.com>
2025-06-04 22:34:55 +0200

    error: Add documentation and structure errors
    Signed-off-by: Joe Pearson <pearjo@protonmail.com>

--- efb/src/error.rs
+++ efb/src/error.rs
@@ -15,40 +15,40 @@
 
 #[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
 pub enum Error {
+    // Errors that can occur while decoding a route:
+    //
     /// The entered flight plan does not include a cruise speed as one of the
     /// first two elements.
     ExpectedSpeedOnFPL,
-
     /// The entered flight plan does not include a cruise level as one of the
     /// first two elements.
     ExpectedLevelOnFPL,
-
-    ExpectedRoute,
-
-    ExpectedAircraft,
-    ExpectedFuelPlanning,
-
+    /// The route to decode includes an element that was not expected.
     UnexpectedRouteElement,
+    /// The route includes a runway at a position that is not next to an
+    /// airport.
     UnexpectedRunwayInRoute,
 
+    // Errors that are related to parsing of input data:
+    //
+    /// The string that should be parsed to create some type is malformed.
     UnexpectedString,
 
-    UnexpectedInput,
-
-    // navigation data
+    // Errors that relate to navigation data:
+    //
     /// The requested identifier is not know.
     UnknownIdent,
 
-    // mass & balance
+    // Errors that originate from the mass & balance planning:
+    //
     /// The number of masses doesn't match the number of stations to which the
     /// masses are assigned.
     UnexpectedMassesForStations,
-
+    /// The number of provided fuel stations doesn't match the aircraft's fuel
+    /// stations.
     UnexpectedNumberOfFuelStations,
-
     /// The planned fuel on ramp exceeds the tank's capacity.
     ExceededFuelCapacityOnRamp,
-
     /// The planned fuel after landing exceeds the tank's capacity.
     ExceededFuelCapacityAfterLanding,
 
